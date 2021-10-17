b1126aae53a3ab78aa203300d6fc98b5b61adf17
sheldonv <velazquezsheldon@gmail.com>
2021-10-16 18:42:32 -0700

    second commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+
+
+const Navbar = () => {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    )
+}
+
+export default Navbar
